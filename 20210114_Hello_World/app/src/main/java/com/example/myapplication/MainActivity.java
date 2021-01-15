d1886979a3ea60be6ccbe7b4e48cd5bba703f4db
bernat <bernat.orellana@gmail.com>
2021-01-15 21:09:03 +0100

    programació de botons

--- 20210114_Hello_World/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 20210114_Hello_World/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.example.myapplication.model.Persona;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * Índex de la persona actualment visualitzada
@@ -20,16 +23,13 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spnProvincia;
     private RadioGroup rdgSexe;
     private Button btnLeft, btnRight, btnOk, btnCancel;
+    private ImageView imvFoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        // recuperem les dades de la persona actual
-        Persona p = Persona.getPersones().get(indexPersona);
-        String nom = p.getNom();
 
         // Cerquem tots els elements de la interfície gràfica i els desem en una cache.
         edtNom = findViewById(R.id.edtNom);
@@ -41,8 +41,75 @@ public class MainActivity extends AppCompatActivity {
         btnRight = findViewById(R.id.btnRight);
         btnCancel = findViewById(R.id.btnCancel);
         btnOk = findViewById(R.id.btnOk);
+        imvFoto = findViewById(R.id.imvFoto);
 
-        edtNom.setText(nom);
+        // Programar esdeveniments
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("HelloWorld", "Han clicat el botó right");
+                // aquest és el codi que s'executa quan faig click
+                indexPersona = (indexPersona+1)%Persona.getPersones().size();
+                mostrarPersonaActual();
+            }
+        });
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                indexPersona--;
+                if(indexPersona<0) indexPersona = Persona.getPersones().size()-1;
+                mostrarPersonaActual();
+            }
+        });
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
 
+
+
+        mostrarPersonaActual();
+     }
+
+    private void mostrarPersonaActual() {
+        // recuperem les dades de la persona actual
+        if(indexPersona>=0 && indexPersona<Persona.getPersones().size())
+        {
+            Persona p = Persona.getPersones().get(indexPersona);
+            String nom = p.getNom();
+            edtNom.setText(nom);
+            edtCognom.setText(p.getCognom());
+            edtTelefon.setText(p.getTelefon());
+            rdgSexe.check(p.isEsHome()?R.id.rdbSexeH:R.id.rdbSexeD);
+            imvFoto.setImageResource(p.getImatgeResourceId());
+        }
+    }
+
+    @Override
+    public void onClick(View sender) {
+        switch (sender.getId()){
+            case  R.id.btnOk:  {
+                saveData();
+                break;
+            }
+            case R.id.btnCancel: {
+                cancelEdit();
+                break;
+            }
+        }
+    }
+
+    private void cancelEdit() {
+        mostrarPersonaActual();
+    }
+
+    private void saveData() {
+        Persona p = getPersonaActual();
+        p.setNom(edtNom.getText().toString());
+        p.setCognom(edtCognom.getText().toString());
+        p.setTelefon(edtTelefon.getText().toString());
+        p.setEsHome( rdgSexe.getCheckedRadioButtonId()==R.id.rdbSexeH );
+    }
+
+    private Persona getPersonaActual() {
+        return Persona.getPersones().get(this.indexPersona);
     }
 }
