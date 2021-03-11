f172bf40bfa5f60c707b7dae6778768b25f386e3
bernat <bernat.orellana@gmail.com>
2021-03-11 16:13:56 +0100

    s

--- 20210311_FragmentsMasterDetail/app/src/main/java/com/example/a20210128_recycler_streetfighter/LlistaFragment.java
+++ 20210311_FragmentsMasterDetail/app/src/main/java/com/example/a20210128_recycler_streetfighter/LlistaFragment.java
@@ -2,18 +2,33 @@ package com.example.a20210128_recycler_streetfighter;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.a20210128_recycler_streetfighter.adapter.CharacterAdapter;
+import com.example.a20210128_recycler_streetfighter.model.Personatge;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link LlistaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LlistaFragment extends Fragment {
+public class LlistaFragment extends Fragment implements CharacterAdapter.OnSelectedItemListener, ActionMode.Callback {
+
+    public static final String TAG = "LLISTA" ;
+    private CharacterAdapter mAdapter;
+    private RecyclerView mRcyPersonatges;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +74,90 @@ public class LlistaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_llista, container, false);
+        View root = inflater.inflate(R.layout.fragment_llista, container, false);
+
+        mRcyPersonatges = root.findViewById(R.id.rcyPersonatges);
+        mRcyPersonatges.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        mAdapter = new CharacterAdapter(this);
+        mRcyPersonatges.setAdapter(mAdapter);
+
+        Toolbar toolbar = root.findViewById(R.id.toolbar);
+
+        return root;
     }
+
+
+
+
+    //-------------------------------------------------
+
+
+
+    private void itemDown() {
+        Log.d("STREETFIGHTER","DOWN");
+        mAdapter.moveDownSelected();
+    }
+
+    private void itemUp() {
+        Log.d("STREETFIGHTER","UP");
+        mAdapter.moveUpSelected();
+    }
+
+    private void itemDelete() {
+        Log.d("STREETFIGHTER","DELETE");
+        mAdapter.deleteSelected();
+    }
+
+    @Override
+    public void onSelectedItem(Personatge seleccionat) {
+        Log.d("STREETFIGHTER","Personatge seleccionat:"+seleccionat);
+
+        // activarem el Contextual Action Mode
+       getActivity().startActionMode(this);
+    }
+
+
+    //---------------------------------------------------------------------
+    // conjunt de mètodes relacions amb el Contextual Action Mode
+    //---------------------------------------------------------------------
+
+    @Override
+    public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+        // Aquí hem de crear el menú contextual
+        MenuInflater inflater = actionMode.getMenuInflater();
+        inflater.inflate(R.menu.menu_main_contextual, menu);
+        updateMenu(menu);
+        return true;
+    }
+
+    private void updateMenu(Menu menu) {
+        menu.findItem(R.id.itmUp).setVisible(mAdapter.getSelectedIndex()>0);
+        menu.findItem(R.id.itmDown).setVisible(mAdapter.getSelectedIndex()<mAdapter.getNumeroPersonatges()-1);
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.itmDelete:    itemDelete(); actionMode.finish();break;
+            case R.id.itmUp:        itemUp(); updateMenu(actionMode.getMenu());break;
+            case R.id.itmDown:      itemDown(); updateMenu(actionMode.getMenu());break;
+        }
+
+        return false;
+    }
+    @Override
+    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {  return false; }
+    @Override
+    public void onDestroyActionMode(ActionMode actionMode) {    }
+    //---------------------------------------------------------------------
+    // FI del conjunt de mètodes relacions amb el Contextual Action Mode
+    //---------------------------------------------------------------------
+
+
+
+
+
+
+
+
 }
