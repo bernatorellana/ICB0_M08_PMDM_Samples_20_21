b72f8e7be3637b2439dd9b22f114412ccbace588
bernat <bernat.orellana@gmail.com>
2021-03-23 15:16:52 +0100

    updated amb imatges

--- 20210322_Paint/app/src/main/java/com/example/a20210322_paint/MainActivity.java
+++ 20210322_Paint/app/src/main/java/com/example/a20210322_paint/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.a20210322_paint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.tools_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch()
+
+        return true;
+    }
 }
